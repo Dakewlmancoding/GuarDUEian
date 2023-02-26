736bade5233d32d6cd0612f5413ee3c4ef0db250
Jack Bell <jack@bellhastold.com>
2023-02-26 18:21:51 -0500

    added the LandRLib and implented lights

--- GuarDUEian.ino
+++ GuarDUEian.ino
@@ -5,7 +5,10 @@ const byte leftEcho = 3;
 const byte rightTrig = 10;
 const byte rightEcho = 11;
 
+const byte lightPin = 8;
+
 const int maxDistCM = 30;
+
 
 //Trigger, echo, maxCM
 UltraSonicDistanceSensor sensorLeft(leftTrig, leftEcho);
@@ -15,19 +18,29 @@ void setup(){
     Serial.begin(115200);
     Serial.println("Hello World");
 
+    pinMode(lightPin, OUTPUT);
+    digitalWrite(lightPin, LOW);
+
 }
+
 
 int leftRead, rightRead;
 int leftReadRAW, rightReadRAW;
 
 bool moveRight, moveLeft;
+bool lightsOn, activeSearch, passiveSearch; //lightsOn is self explanatory. Active search is the state where the lights are on and it is actively moving to find the player. Passive Search is when the lights are on and the head is idely moving. Both search modes are controlled by timers.
+
 
 
 void loop(){
 
     getLRDistReadings();
+    checkMoveHead();
     moveHead();
+    
 }
+
+
 
 void getLRDistReadings() {
     leftReadRAW = sensorLeft.measureDistanceCm();
@@ -50,8 +63,8 @@ void getLRDistReadings() {
 }
 
 
-// Uses LRDistReadings to move head left or right 
-void moveHead() {
+// Uses LRDistReadings to set moveRight and moveLeft
+void checkMoveHead() {
         if ((leftRead > 0) or (rightRead > 0))
     {
         if((leftRead < 0) and (rightRead > 0)) {
@@ -65,16 +78,27 @@ void moveHead() {
             moveRight = false;
         }
     }
+}
 
+//code to move the head CURRENTLY TEST CODE
+void moveHead(){
     if (moveLeft or moveRight) {
+        if (not lightsOn){
+            lightsOn = true;
+            digitalWrite(lightPin, HIGH);
+        }
+        
         if (moveLeft) {
             Serial.println("Moving Left!");
         } else if (moveRight) {
             Serial.println("Moving Right!");
         }
         
-        
     } else {
+        if (lightsOn){
+            lightsOn = false;
+            digitalWrite(lightPin, LOW);
+        }
         Serial.print("Left: ");
         Serial.print(leftRead);
 
@@ -82,4 +106,3 @@ void moveHead() {
         Serial.println(rightRead);
     }
 }
-    
