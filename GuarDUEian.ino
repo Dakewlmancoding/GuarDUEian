deac888efb99c5c5856bee09f6d46bd0391ee43d
Jack Bell <jack@bellhastold.com>
2023-03-03 20:51:16 -0500

    Added Servo code. Is brokey :(

--- GuarDUEian.ino
+++ GuarDUEian.ino
@@ -14,6 +14,8 @@ unsigned long playerLastSightedTime; //time in microseconds since it's seen the 
 
 //timers start at 6 b/c servos!!
 DueTimer threatTimer = DueTimer(6); //the timer to switch the threat level
+DueTimer idleTimer = DueTimer(7); //the timer to switch the threat level
+char idleDirection;
 
 /************************************************** FUNCTIONS ***************************************************/
 
@@ -23,14 +25,35 @@ void updateThreat(){
     }
 }
 
+void updateIdle(){
+    switch (random(0,2)) {
+    case 0:
+        idleDirection = 'n';
+    break;
+    
+    case 1:
+        idleDirection = 'l';
+    break;
+
+    case 2:
+        idleDirection = 'r';
+    break;
+
+    default:
+        break;
+    }
+}
+
 /************************************************** BUILT IN FUNCS ***************************************************/
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Hello World");
-    
+
     //timers
     threatTimer.attachInterrupt(updateThreat);
+    idleTimer.attachInterrupt(updateIdle);
+    idleTimer.start(5000000);
 
     setupServo();
     setupLights();
@@ -42,6 +65,7 @@ void loop() {
     beginLoopThreatLevel = threatLevel;
     updateLRDistReadings();
     checkMoveHead();
+
     if (canSeePlayer()){
         threatTimer.stop();
         threatTimer.start(20000000);
@@ -56,14 +80,21 @@ void loop() {
     switch (threatLevel){
     case 0:
         eyeOff();
+        moveHead('n');
     break;
-    
+
     case 1:
         eyeOn(200);
+        moveHead(idleDirection);
     break;
 
     case 2:
         eyeOn(200);
+        if (moveRight){
+            moveHead('r');
+        } else if (moveLeft){
+            moveHead('l');
+        }
     break;
 
     default:
--- Libraries/movingHead.h
+++ Libraries/movingHead.h
@@ -12,22 +12,26 @@ Servo headServo;
 /************************************************** FUNCTIONS ***************************************************/
 
 void setupServo() {
-    headServo.attach(9); // attaches the servo on pin 9
+    headServo.attach(5); // attaches the servo on pin 5
 }
 
 char lastDirection;
 
 //moves the servo in direction. l = counterclockwise, r = clockwise, n = don't move
 void moveHead(char direction){
+    Serial.print(direction);
+    Serial.println(" in MoveHead");
     if (not (direction == lastDirection)){
         headServo.writeMicroseconds(1500);
         switch (direction){
         case 'l':
             headServo.writeMicroseconds(1400);
+            Serial.println("Moving Left");
         break;
         
         case 'r':
             headServo.writeMicroseconds(1600);
+            Serial.println("Moving Right");
         break;
 
         default:
