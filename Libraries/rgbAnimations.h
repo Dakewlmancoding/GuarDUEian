2202293adfa94f12697ef2aaad07a82ce8ca9de9
Jack Bell <jack@bellhastold.com>
2023-03-04 21:50:15 -0500

    changed light pin

--- Libraries/rgbAnimations.h
+++ Libraries/rgbAnimations.h
@@ -3,7 +3,7 @@
 
 /************************************************** VARIABLES ***************************************************/
 
-const int eyePin = 8; // Eye data in pin
+const int eyePin = 9; // Eye data in pin
 const int numLeds = 57;
 const int eyeBrightness = 10;
 Adafruit_NeoPixel eyeRings = Adafruit_NeoPixel(numLeds, eyePin, NEO_GRB + NEO_KHZ800);
